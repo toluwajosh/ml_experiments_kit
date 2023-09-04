42b28bd42630d8d6b5e63698803fc2e9b2cc477e
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2023-09-04 18:12:57 +0900

    setup factories

--- src/runners/training_runner.py
+++ src/runners/training_runner.py
@@ -1,30 +1,20 @@
-from typing import Optional
-
-import torch
-
-from src.datasets.protocol import Dataset
+from src.operations.training import Training
 from src.settings.run_config import ExpConfig
-from src.tracking.protocol import Tracker
 
 
 class TrainingRunner:
     def __init__(
         self,
         config: ExpConfig,
-        model: torch.nn.Module,
-        criterion: torch.nn.Module,
-        optimizer: torch.optim.Optimizer,
-        tracker: Tracker,
-        train_dataset: Dataset,
-        test_dataset: Optional[Dataset],
     ) -> None:
         self.config = config
-        self.model = model
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.tracker = tracker
-        self.train_dataset = train_dataset
-        self.test_dataset = test_dataset
+        
+        # Assets are loaded from factories
+        # load previous model if available
+        # load previous optimizer if available
+        # load previous tracker if available
+        # set up training parameters
+        self.training: Training
 
     def run(self) -> None:
-        pass
+        self.training.run()
